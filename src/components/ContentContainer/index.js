ba347c318d5725903bf25b97ef648725867ef0a8
AleksHNZ <86948893+AleksHNZ@users.noreply.github.com>
2022-01-23 13:05:58 +0400

    first commit

---
+++ src/components/ContentContainer/index.js
@@ -0,0 +1,87 @@
+import TopNavigation from '../TopNavigation';
+import { BsPlusCircleFill } from 'react-icons/bs';
+// import { useState } from 'react';
+
+const ContentContainer = () => {
+  return (
+    <div className='content-container'>
+      <TopNavigation />
+      <div className='content-list'>
+        <Post
+          name='Ali'
+          timestamp='one week ago'
+          text={`Hey, guys. Who interested to building some brand new project?`}
+        />
+        <Post name='Leon' timestamp='one week ago' text={`Lorem ipsum dolor. `} />
+        <Post name='Jill' timestamp='5 days ago' text={`Lorem.`} />
+        <Post
+          name='Ellie'
+          timestamp='4 days ago'
+          text={`I want, but not today. Sorry :). `}
+        />
+        <Post
+          name='Chris'
+          timestamp='4 days ago'
+          text={`Sure, why not.`}
+        />
+        <Post
+          name='Claire'
+          timestamp='2 days ago'
+          text={`I'm kinda busy today.`}
+        />
+        <Post
+          name='Albert'
+          timestamp='22 hours ago'
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. ☺️ `}
+        />
+        <Post
+          name='Rebecca'
+          timestamp='3 hours ago'
+          text={`Give me five minutes. I will right back.`}
+        />
+        <Post
+          name='H.U.N.K'
+          timestamp='Just now'
+          text={`Can you say exactly what you want to build?`}
+        />
+      </div>
+      <BottomBar />
+    </div>
+  );
+};
+
+const BottomBar = () => (
+  <div className='bottom-bar'>
+    <PlusIcon />
+    <input type='text' placeholder='Enter message...' className='bottom-bar-input' />
+  </div>
+);
+
+const Post = ({ name, timestamp, text }) => {
+
+  const seed = Math.round(Math.random() * 100);
+  return (
+    <div className={'post'}>
+      <div className='avatar-wrapper'>
+        <img src={`https://avatars.dicebear.com/api/open-peeps/${seed}.svg`} alt='' className='avatar' />
+      </div>
+
+      <div className='post-content'>
+        <p className='post-owner'>
+          {name}
+          <small className='timestamp'>{timestamp}</small>
+        </p>
+        <p className='post-text'>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const PlusIcon = () => (
+  <BsPlusCircleFill
+    size='22'
+    className='text-green-500 dark:shadow-lg mx-2 dark:text-primary'
+  />
+);
+
+export default ContentContainer;
